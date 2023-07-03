1ead49d5b290a22f40a09b6480340c7ee4615994
Pavel Grigorev <pavel.grigorev.1990@mail.ru>
2023-07-04 01:24:33 +0300

    added Main

---
+++ Main.java
@@ -0,0 +1,48 @@
+package ООП.Lesson_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import ООП.Lesson_1.Animal0;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Slon", 32, "true", "White",
+        "Mammal", new Owner("Stas"), 4);
+        Animal animal1 = new Animal("Black", "Bird", 2);
+        System.out.println(animal.getAge());
+        animal.setAge(3);
+        System.out.println(animal.getAge());
+        System.out.println(animal);
+        Animal0 animal0 = new Animal0(null, null, null, null, null, null, null);
+        animal0.getAge();
+        System.out.println(animal0);
+        Animal cat = new Animal0();
+        List <Animal> animals = new ArrayList<>();
+        animals.add(new Dog(null, null, null));
+        animals.add(new Cat(null, null, null));
+        animals.add(cat);
+        animals.add(animal);
+        animals.add(animal1);
+        System.out.println(animals);
+        // for (Animal item : animals) {
+        //     if (item instanceof Cat) {
+        //     ((Cat)item).FIndFood(3);
+        //     ((Cat)item).FIndFood();
+        //     }
+        // }
+        Cat barsik = new Cat(null,null,null);
+        barsik.Hunt();
+        barsik.toFly();
+        Dolphin flipper = new Dolphin(null, null, null);
+        flipper.toFly();
+        flipper.toRun();
+        flipper.toSwim();
+        Pigeon pig = new Pigeon(null, null, null);
+        pig.toSwim();
+        pig.toFly();
+        Crocodile croc = new Crocodile(null, null, null);
+        croc.toFly();
+
+        
+    }
+}
